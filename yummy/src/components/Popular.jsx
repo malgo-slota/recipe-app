39fe54989ae7180696d02fcbe27eefe0d5d12c1a
malgo-slota <malgo.slota@gmail.com>
2022-07-06 11:03:38 +0200

    fetch API

--- yummy/src/components/Popular.jsx
+++ yummy/src/components/Popular.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+
+
 function Popular() {
+
+    const[popularRecipes, setPopularRecipes] = useState([]);
+
+    useEffect(() => {   
+        getPopularRecipes();
+    },[]);
+
+    const getPopularRecipes = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SECRET_API_KEY}&number=9`
+            );
+        const data = await api.json();
+        setPopularRecipes(data.recipes);
+    };
+
     return (
         <div>
-            Popular recipes
+            <h1>Popular recipes</h1>
+            {popularRecipes.map(recipe => {
+                return (
+                    <div key={recipe.id}>
+                        <p>{recipe.title}</p>
+                        <img src={`${recipe.image}`} alt="recipe img"/>
+                    </div>
+                );
+            })}
         </div>
     );
 }
